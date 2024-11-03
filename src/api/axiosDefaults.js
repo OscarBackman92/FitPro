873533b54b944870eb8165553edb4863ec9a43eb
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-11-03 09:44:20 +0100

    chore: Add environment configuration

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "https://fitnessapi-d773a1148384.herokuapp.com";
+// Use development URL if in development mode
+const baseURL = process.env.NODE_ENV === "development" 
+  ? process.env.REACT_APP_DEV_API_URL 
+  : process.env.REACT_APP_API_URL;
 
-// Define defaults
-axios.defaults.baseURL = API_URL;
-axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.baseURL = baseURL;
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-// Create separate instances for requests and responses
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
