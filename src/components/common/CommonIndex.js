65163b0563fb86fbc81c216d06fe8c1729b2a74f
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-11-07 08:04:36 +0100

    fix:Move commonfiles to better place and hooksIndex.js

--- src/components/common/CommonIndex.js
+++ src/components/common/CommonIndex.js
@@ -1,3 +1,8 @@
+export { default as Asset } from './Asset';
+export { default as Avatar } from './Avatar';
+export { default as MoreDropdown } from './MoreDropdown';
+export { default as NavBar } from './NavBar';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as NotFound } from './NotFound';
 export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as ErrorMessage } from './ErrorMessage';
